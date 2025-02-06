0feea0361a64fefdf40c7782695ab45b1ad6c725
SofiyaLo <sonyalopatina01@gmail.com>
2025-02-06 13:20:40 +0300

    Сделаны функции для взвешенного и невзвешенного чтения файлов. Сделан функции для PCA по видам и генам. Обновлены примеры

--- PoFF_analysis.py
+++ PoFF_analysis.py
@@ -6,33 +6,77 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
-# Функция для чтения файла Protienortho
-def ortho_reader(file):
-    ortho_df = pd.read_csv(file, sep='\t')
-    categorical_columns = ortho_df.columns[3:19]
 
-    def replace_gene_values(value):
-        if value == '*':
-            return 0  # Заменить * на 0
-        elif isinstance(value, str):
-            return len(value.split(','))  # Количество генов (разделенных запятыми)
-        return 0  # Если значение не строка, заменить на 0
+def ortho_unweight_reader(file, threshold):
+    """Функция читает файл результата ProteinOrtho и убирает неинформативные колонки.
+        Каждое значение гена заменяет бинарно на наличие/отсутствие и фильтрует по заданному порогу
+        threshold, обозначащем количество ортологов в семействе генов"""
 
-    # Применение функции replace_gene_values ко всем значениям в столбцах с генами
-    ortho_df[categorical_columns] = ortho_df[categorical_columns].applymap(replace_gene_values)
-    return ortho_df
+    ortho_df = pd.read_csv(file, sep='\t').iloc[:, 3:19]
+    ortho_df = ortho_df.map(lambda x: 0 if x == '*' else 1)
+    ortho_df['zero_count'] = (ortho_df == 0).sum(axis=1)
 
-def PCA_graph(dataset, n_components=2):
-    # Map "Missing" на 0 и "Complete" на 1
-    data_mapped = dataset.map(lambda x: 0 if x == '*' else 1 if x == 'Complete' else x)
-    data_mapped = data_mapped.transpose()
-    data_mapped["spic_number"] = [i for i in range(1, len(data_mapped) + 1)]
+    # Оставляем только строки, в которых количество нулей >= threshold
+    filtered_df = ortho_df[ortho_df['zero_count'] <= threshold].drop(columns=['zero_count'])  # Удалим столбец с количеством нулей
+
+    return filtered_df
+
+
+def ortho_weight_reader(file, threshold):
+    """Функция читает файл результата ProteinOrtho и убирает неинформативные колонки.
+        Каждое значение гена заменяет количеством паралогов и фильтрует по заданному порогу
+        threshold, обозначащем количество ортологов в семействе генов"""
+
+    ortho_df = pd.read_csv(file, sep='\t').iloc[:, 3:19]
+
+    ortho_df = ortho_df.map(lambda x: 0 if x == '*' else len(x.split(',')))
+    ortho_df['zero_count'] = (ortho_df == 0).sum(axis=1)
+
+    # Оставляем только строки, в которых количество нулей >= пороговому значению
+    filtered_df = ortho_df[ortho_df['zero_count'] <= threshold].drop(columns=['zero_count'])  # Удалим столбец с количеством нулей
+
+    return filtered_df
+
+def PCA_graph_spic(dataset, n_components=2):
+    """Функция принимает датафрейм из ProteinOrtho преобразованный функциями ortho_weight_reader
+     или ortho_unweight_reader. Возвращает результат PCA по семействам генов включая график"""
+
+    #Транспонируем датасет, чтобы гены стали строками а семейства колонками
+    dataset = dataset.transpose()
+    #Добавляем порядковый номер вида
+    dataset["spic_number"] = [i for i in range(1, len(dataset) + 1)]
 
     # Инициализируем PCA с нужным количеством компонент
     pca = PCA(n_components=n_components)
 
     # Обучаем PCA на маппированных нормализованных данных
-    pca_result = pca.fit_transform(data_mapped.iloc[:, :-1])
+    pca_result = pca.fit_transform(dataset.iloc[:, :-1])
 
+    df = pd.DataFrame({"PC1": pca_result[:, 0],
+                       "PC2": pca_result[:, 1],
+                       "spic_number": dataset["spic_number"]})
 
+    sns.scatterplot(x="PC1", y="PC2", hue="spic_number", data=df, alpha=0.75, palette="bright")
+    plt.show()
 
+def PCA_graph_genes(dataset, n_components=2):
+    """Функция принимает датафрейм из ProteinOrtho преобразованный функциями ortho_weight_reader
+     или ortho_unweight_reader. Возвращает результат PCA по видам включая график"""
+
+    #Добавляем порядковый номер семейства
+    dataset["gene_number"] = [i for i in range(1, len(dataset) + 1)]
+
+    # Инициализируем PCA с нужным количеством компонент
+    pca = PCA(n_components=n_components)
+
+    # Обучаем PCA на маппированных нормализованных данных
+    pca_result = pca.fit_transform(dataset.iloc[:, :-1])
+
+    df = pd.DataFrame({"PC1": pca_result[:, 0],
+                       "PC2": pca_result[:, 1],
+                       "gene_number": dataset["gene_number"]})
+
+    sns.scatterplot(x="PC1", y="PC2", hue="gene_number", data=df, alpha=0.75, palette="bright")
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=6)
+
+    plt.show()
--- run_crosser.py
+++ run_crosser.py
@@ -1,11 +1,15 @@
 import pr_func
-
+import PoFF_analysis
 
 genome_folder = r'C:\Users\Professional\PycharmProjects\Protein-crossing\test_data_for_sonya_short_summaries'
 table_sp = r'C:\Users\Professional\PycharmProjects\Protein-crossing\table_new_species.csv'
-table_gene_comb = pr_func.gene_comb(genome_folder, table_sp)
-missing_list = pr_func.list_of_missing_genes(table_gene_comb)
+#table_gene_comb = pr_func.gene_comb(genome_folder, table_sp)
+#missing_list = pr_func.list_of_missing_genes(table_gene_comb)
+pr_ortho_tsv = r'C:\Users\Professional\PycharmProjects\Protein-crossing\Prot_ortho_results\very_sensitive.proteinortho.tsv'
 
-
-pr_func.plot_clustered_heatmap(table_gene_comb)
-print(pr_func.PCA_graph(pr_func.gene_comb(genome_folder, table_sp)))
+w_ortho_df = PoFF_analysis.ortho_weight_reader(pr_ortho_tsv, 2)
+unw_ortho_df = PoFF_analysis.ortho_unweight_reader(pr_ortho_tsv, 2)
+print(PoFF_analysis.PCA_graph_spic(w_ortho_df))
+print(PoFF_analysis.PCA_graph_genes(w_ortho_df))
+print(PoFF_analysis.PCA_graph_spic(unw_ortho_df))
+print(PoFF_analysis.PCA_graph_genes(unw_ortho_df))
